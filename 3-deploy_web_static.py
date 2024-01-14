65218c2138f39e3a9308fc3ed944f9c86645df19
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-01-14 09:19:35 +0000

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -15,7 +15,9 @@ def do_pack():
     date_time = date_time.strftime('%Y%m%d%H%M%S')
 
     try:
-        local('mkdir -p versions')
+        if path.isdir("versions") is False:
+            local('mkdir -p versions')
+
         local('tar -czvf versions/web_static_{}.tgz web_static/'
               .format(date_time))
 
