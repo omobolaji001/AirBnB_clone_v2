b5dcdcb6c1636f508991164516260e4dd310f1a3
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-01-28 22:09:07 +0100

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,3 +70,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session)
+
+    def close(self):
+        """Closes the current session"""
+        self.__session.remove()
