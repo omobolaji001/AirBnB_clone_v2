c9f23c780efa20d6a1a7c1af6e9141c7065efdf4
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-01-29 01:25:26 +0100

    update

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays HTML page with the list of all cities by states in DBstorage
+
+    States and Cities are sorted by `name`
+    """
+    states = storage.all("State")
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def tearDown(exc):
+    """Closes the current Sql session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
